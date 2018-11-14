f617c07a1b0d7e9213228af8f178612771c4cdbb
salmamrais <salmamrais@gmail.com>
2018-11-14 01:14:03 +0100

    Fixed the sponsors layout and links and upgraded the Home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  travaillonsSurList = ['l\'harmonie', 'l\'unité', 'l\'excellence', 'la qualité'];
+  travaillonsSur;
+
   constructor() { }
 
+
   ngOnInit() {
+    let i=0;
+    const timerId = setInterval(() => this.travaillonsSur = this.travaillonsSurList[++i % this.travaillonsSurList.length], 1000);
+
   }
 
 }
+
+
